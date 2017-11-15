eeac70a6c1dac339f2213cbf03d36b3d681f41e9
vishant.singh <vishant.singh@sofocle.com>
2017-11-15 17:55:06 +0530

    embark change

--- embark_demo/app/js/index.js
+++ embark_demo/app/js/index.js
@@ -1,117 +1,37 @@
 $(document).ready(function() {
   var currentToken;
-  // if(localStorage.getItem('ABI') && localStorage.getItem('ADDRESS')) {
-  //   var ABI = currentToken = JSON.parse(localStorage.getItem('ABI'));
-  //   var Adress = JSON.parse(localStorage.getItem('ADDRESS'));
-  //   currentToken = web3.eth.contract(ABI).at(Adress);
-  // }
-
+  var value;
   $("#deploy button.set").click(function() {
-    MileStoneTracker.deploy().then(function(deployedToken) {
+    console.log('hii');
+    SimpleStorage.deploy().then(function(deployedToken) {
       currentToken = deployedToken;
-      // localStorage.setItem('ABI', JSON.stringify(deployedToken.abi));
-      // localStorage.setItem('ADDRESS', JSON.stringify(deployedToken.address));
-      // currentToken = web3.eth.contract(deployedToken.abi).at(deployedToken.address);
+      console.log(deployedToken.address);
+      console.log(SimpleStorage.abi);
     });
   });
   $("#registr button.set").click(function() {
-    var value = $("#setemplate").val();
-    currentToken.setTemplates(value).then(function(err, res){
-      if(err) console.log(err);
-      console.log(res);
+    value = parseInt($("#4").val());
+    console.log(value);
+    currentToken.RegisterUser(value).then(function(reg) {
+      console.log(reg);
     });
   });
   $("#Account button.set").click(function() {
-    currentToken.getTemplate().then(function(err,res) {
-      if(err) console.log(err);
-      console.log(res);
+    value = parseInt($("#5").val());
+    console.log(value);
+    currentToken.GetUserAccount(value).then(function(reg) {
+      console.log(reg);
     });
   });
   $("#token button.set").click(function() {
-    var value = $("#setmile1").val();
-    var value2 =$("#setmile2").val();
-    var value3 = $("#setmile3").val();
-    currentToken.setMilestones(value ,value2 , value3).then(function(err,res) {
-      if(err) console.log(err);
-      console.log(res);
-    });
-  });
-  $("#tempid button.set").click(function() {
-    var value = $("#tempidvalue").val();
-    currentToken.getMilestonesByTemp_id(value).then(function(err,res) {
-      if(err) console.log(err);
-      console.log(res);
-    });
-  });
-  $("#mileid button.set").click(function() {
-    var value = $("#mileidvalue").val();
-    currentToken.getMilestonesByMile_id(value).then(function(err,res) {
-      if(err) console.log(err);
-      console.log(res);
-    });
-  });
-  $("#customer button.set").click(function() {
-    var value1 = $("#customervalue1").val();
-    var value2 = $("#customervalue2").val();
-    console.log(value1);
+    value = parseInt($("#1").val());
+    var value2 = parseInt($("#2").val());
+    var value3 = parseInt($("#3").val());
+    console.log(value);
     console.log(value2);
-    currentToken.set_customer(value1,value2).then(function(err,res) {
-      if(err) console.log(err);
-      console.log(res);
-    });
-  });
-  $("#serviceprovider button.set").click(function() {
-    var value1 = $("#serviceprovidervalue1").val();
-    var value2 = $("#serviceprovidervalue2").val();
-    console.log(value1);
-    console.log(value2);
-    currentToken.set_ServiceProvider(value1,value2).then(function(err,res) {
-      if(err) console.log(err);
-      console.log(res);
-    });
-  });
-  $("#admin button.set").click(function() {
-    var value1 = $("#adminvalue1").val();
-    var value2 = $("#adminvalue2").val();
-    console.log(value1);
-    console.log(value2);
-    currentToken.set_Admin(value1,value2).then(function(err,res) {
-      if(err) console.log(err);
-      console.log(res);
-    });
-  });
-  $("#checkPayment button.set").click(function() {
-    var value1 = $("#checkPaymentvalue1").val();
-    // var value2 = parseInt($("#checkPaymentvalue2").val());
-    // var eth = 1000000000000000000;
-    var value2 = 1000000000000000000;
-    // value2 = eth*value2;
-    console.log(value1);
-    console.log(value2);
-    currentToken.check_Payment(value1,value2,{value:value2}).then(function(err,res) {
-      if(err) console.log(err);
-      console.log(res);
-    });
-  });
-  $("#checkStatus button.set").click(function() {
-    var value1 = $("#checkStatusvalue1").val();
-    currentToken.check_status(value1).then(function(err,res) {
-      if(err) console.log(err);
-      console.log(res);
-    });
-  });
-  $("#markmilestone button.set").click(function() {
-    var value1 = $("#markmilestonevalue1").val();
-    currentToken.markMilestoneComplete(value1).then(function(err,res) {
-      if(err) console.log(err);
-      console.log(res);
-    });
-  });
-  $("#ApproveCustomer button.set").click(function() {
-    var value1 = $("#ApproveCustomervalue1").val();
-    currentToken.approveByCustomer(value1).then(function(err,res) {
-      if(err) console.log(err);
-      console.log(res);
+    console.log(value3);
+    currentToken.Send(value ,value2 , value3).then(function(reg) {
+      console.log(reg);
     });
   });
 });
